7ca843a91e06782110d89cd3204eb6f4a1d3eb8f
CampaUTN <rodrigocampassi@hotmail.com>
2016-10-18 09:43:26 -0300

    Avanzo login

--- src/ClinicaFrba/ClinicaFrba/Login/Login.cs
+++ src/ClinicaFrba/ClinicaFrba/Login/Login.cs
@@ -21,6 +21,21 @@ namespace ClinicaFrba.Login
         private void button1_Click(object sender, EventArgs e)
         {
             var conexion = DBConnection.getInstance().getConnection();
+
+            SqlCommand comando = new SqlCommand("CLINICA.spLogin", conexion);
+            /* Tiene que ser un SP creado en el script que verifique del lado del motor la cantidad de intentos */
+            comando.CommandType = CommandType.StoredProcedure;
+            /* Aca van los parametros del SP, lo comento porque no se bien que mas va a necesitar
+             * comando.Parameters.Add(new SqlParameter("@usuario",this.textUsuario.Text));
+             * comando.Parameters.Add(new SqlParameter("@contrasenia",this.textContrasenia.Text));
+             */
+            conexion.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+            // List key value?
+            if (!reader.HasRows){
+                MessageBox.Show("No existe el usuario '" + this.textUsuario + "' en el sistema", "Error de Login", MessageBoxButtons.OK);
+            }
+
         }
     }
 }
