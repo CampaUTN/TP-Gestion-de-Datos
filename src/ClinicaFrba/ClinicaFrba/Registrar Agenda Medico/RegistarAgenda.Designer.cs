cab15583f2f9e2c0ef9cd1d62f9e083d937ebff1
lucasesposito <espositolucas95@gmail.com>
2016-10-30 18:06:17 -0300

    empiezo alta de horarios

--- src/ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/RegistarAgenda.Designer.cs
+++ src/ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/RegistarAgenda.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ClinicaFrba.Registrar_Agenta_Medico
+﻿namespace ClinicaFrba.Registrar_Agenda_Medico
 {
     partial class RegistarAgenda
     {
@@ -28,11 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.selectorFecha = new System.Windows.Forms.DateTimePicker();
+            this.botonCancelar = new System.Windows.Forms.Button();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectorFecha
+            // 
+            this.selectorFecha.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.selectorFecha.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectorFecha.Checked = false;
+            this.selectorFecha.CustomFormat = "";
+            this.selectorFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.selectorFecha.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
+            this.selectorFecha.Location = new System.Drawing.Point(140, 94);
+            this.selectorFecha.Margin = new System.Windows.Forms.Padding(5);
+            this.selectorFecha.MaxDate = new System.DateTime(2016, 10, 26, 0, 0, 0, 0);
+            this.selectorFecha.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.selectorFecha.Name = "selectorFecha";
+            this.selectorFecha.RightToLeftLayout = true;
+            this.selectorFecha.Size = new System.Drawing.Size(130, 20);
+            this.selectorFecha.TabIndex = 14;
+            this.selectorFecha.Value = new System.DateTime(2016, 10, 26, 0, 0, 0, 0);
+            this.selectorFecha.ValueChanged += new System.EventHandler(this.selectorFecha_ValueChanged);
+            // 
+            // botonCancelar
+            // 
+            this.botonCancelar.Location = new System.Drawing.Point(12, 227);
+            this.botonCancelar.Name = "botonCancelar";
+            this.botonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.botonCancelar.TabIndex = 21;
+            this.botonCancelar.Text = "Cancelar";
+            this.botonCancelar.UseVisualStyleBackColor = true;
+            this.botonCancelar.Click += new System.EventHandler(this.botonCancelar_Click);
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Location = new System.Drawing.Point(197, 227);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(75, 23);
+            this.AceptarButton.TabIndex = 22;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            this.AceptarButton.Click += new System.EventHandler(this.AceptarButton_Click);
+            // 
+            // RegistarAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.botonCancelar);
+            this.Controls.Add(this.selectorFecha);
+            this.Name = "RegistarAgenda";
+            this.Text = "Alta Agenda Profesional";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        protected System.Windows.Forms.DateTimePicker selectorFecha;
+        protected System.Windows.Forms.Button botonCancelar;
+        protected System.Windows.Forms.Button AceptarButton;
     }
 }
--- src/ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/RegistarAgenda.cs
+++ src/ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/RegistarAgenda.cs
@@ -1,20 +1,62 @@
-﻿using System;
+﻿using ClinicaFrba.Utilidades;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ClinicaFrba.Registrar_Agenta_Medico
+namespace ClinicaFrba.Registrar_Agenda_Medico
 {
     public partial class RegistarAgenda : Form
     {
-        public RegistarAgenda()
-        {
+        protected int profesional_id;
+        protected int especialidad_id;
+        protected DateTime fechaHora;
+        protected Horario horario;
+
+        protected Logger logErrores;
+
+        public RegistarAgenda() {
             InitializeComponent();
+            this.logErrores = new Logger();
+        }
+
+        private void selectorFecha_ValueChanged(object sender, EventArgs e) {}
+
+        private void botonCancelar_Click(object sender, EventArgs e) {
+            if (MessageBox.Show("¿Esta seguro que desea cancelar?", "Cancelar", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                this.Close();
+            }
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e) {
+            //chequeo que todos los campos se hayan completado
+            if (!faltaCompletarDatos()) {
+                //chequeo que los datos sean correctos
+                this.validarDatosIngresados();
+                if (!this.logErrores.huboErrores()) {
+                    //metodo que sobreescribe Modificacion
+                    cargarHorario();
+                } else {
+                    MessageBox.Show("Error en el ingreso de datos:\n" + this.logErrores.mostrarLog() + "\nCompruebe que haya ingresado los datos en forma correcta y vuelva a intentarlo.", "Error", MessageBoxButtons.OK);
+                }
+                this.logErrores.resetear();
+            } else {
+                MessageBox.Show("Debe completar todos los datos del horario para continuar", "Aviso", MessageBoxButtons.OK);
+
+            }
+        }
+
+
+        public virtual void cargarHorario(){
+            horario = new Horario(profesional_id, especialidad_id, fechaHora);
+        }
+
+        private bool faltaCompletarDatos() {
+            return cajasTexto.Any(cajita => cajita.Text.Length.Equals(0));
+        }
+
+        private void validarDatosIngresados() {
+            // TODO
         }
     }
 }
