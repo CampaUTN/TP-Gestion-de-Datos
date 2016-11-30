adcd6696aa021bca053a3e0724313617acab7480
emitolaba95 <emi.tolaba95@gmail.com>
2016-11-30 00:29:24 -0300

    Saco trigger que verifica los intentos del usuario

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -657,7 +657,12 @@ GO
 CREATE PROCEDURE GEDDES.darDeBaja(@user BIGINT)
 AS
 BEGIN
-	UPDATE GEDDES.Usuarios
+	DECLARE @intentos INT
+	SET @intentos = (SELECT TOP 1 usua_intentos FROM GEDDES.Usuarios WHERE usua_id = @user)
+	IF @intentos = 0		
+		RAISERROR('El usuario ya ha sido dado de baja anteriormente',16,2)
+	ELSE 		
+		UPDATE GEDDES.Usuarios
 		SET usua_intentos = 0
 		WHERE usua_id = @user
 END
@@ -904,21 +909,4 @@ AS
 			WHERE usua_id IN (SELECT d.usua_id FROM deleted d)
 	
 	END
-GO
-
-
-USE GD2C2016;
-GO
-CREATE TRIGGER GEDDES.verificarBaja ON GEDDES.Usuarios INSTEAD OF UPDATE 
-AS
-	BEGIN
-		IF EXISTS (SELECT* FROM GEDDES.Usuarios u, inserted i WHERE u.usua_id = i.usua_id AND u.usua_intentos = 0)		 
-			RAISERROR('El usuario ya ha sido dado de baja anteriormente',16,2)
-		ELSE		 
-			UPDATE GEDDES.Usuarios 
-			SET usua_intentos = inserted.usua_intentos
-			FROM inserted
-			WHERE inserted.usua_id = GEDDES.Usuarios.usua_id
-		
-	END 
 GO
